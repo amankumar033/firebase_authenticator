a975d3c40ab8152545777d0f3a899f23a98b7648
amankumar033 <166852922+amankumar033@users.noreply.github.com>
2025-03-09 21:55:55 +0530

    authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBAcBLg0QgNZfAUGh44rsOlmYp4SqXI8bY",
   authDomain: "otp-authenticator-29c81.firebaseapp.com",
@@ -18,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export {app, auth};
